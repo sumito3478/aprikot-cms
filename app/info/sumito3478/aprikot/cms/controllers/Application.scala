dfd14a8a509cb66efecd296e5b5b6f49d222ca90
sumito3478 <sumito3478@gmail.com>
2013-02-07 03:44:47 +0900

    Rename package `controllers` and `views`

--- app/info/sumito3478/aprikot/cms/controllers/Application.scala
+++ app/info/sumito3478/aprikot/cms/controllers/Application.scala
@@ -1,7 +1,10 @@
-package controllers
+package info.sumito3478.aprikot.cms.controllers
 
 import play.api._
 import play.api.mvc._
+
+import info.sumito3478.aprikot._
+import info.sumito3478.aprikot.cms._
 
 object Application extends Controller {
   
