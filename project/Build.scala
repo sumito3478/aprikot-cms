86b46ef77b90e3899c3674f5524cd1f4eb6a5073
sumito3478 <sumito3478@gmail.com>
2013-02-12 16:59:54 +0900

    Add aprikot and aprikot-sbt to library/plugin deps

--- project/Build.scala
+++ project/Build.scala
@@ -2,19 +2,26 @@ import sbt._
 import Keys._
 import play.Project._
 
+import info.sumito3478.aprikot.sbt._
+
 object ApplicationBuild extends Build {
 
   val appName         = "aprikot-cms"
   val appVersion      = "0.0.1-SNAPSHOT"
 
   val appDependencies = Seq(
+    "info.sumito3478" %% "aprikot" % "0.2.+",
     "com.micronautics" % "securesocial" % "2.1-RC4a" withSources(),
     jdbc,
     anorm
   )
 
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
+  val main = play.Project(
+    appName,
+    appVersion,
+    appDependencies).settings(StandardProject.newSettings: _*
+  ).settings(
     resolvers += Resolver.url(
       "play-plugin-releases",
       new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases"))(
