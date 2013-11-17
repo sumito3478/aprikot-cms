ba7ed5f80ecd9da9ebeda9062e409980e114956b
sumito3478 <sumito3478@gmail.com>
2013-11-17 17:36:44 +0900

    [from now] 2013/11/17 17:36:41
    diff --git a/project/project/Plugins.scala b/project/project/Plugins.scala
    index 5fe936d..2d213e0 100644
    --- a/project/project/Plugins.scala
    +++ b/project/project/Plugins.scala
    @@ -6,8 +6,8 @@ object Plugins extends Build {
    
       lazy val plugins = Project("plugins", file(".")).settings(
         logLevel := Level.Warn,
    -    resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
    -    addSbtPlugin("play" % "sbt-plugin" % "2.2.1")
    +    resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
    +    addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.2.1"),
         addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.2.1"),
         addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.2"),
         addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.2"))

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -6,8 +6,8 @@ object Plugins extends Build {
 
   lazy val plugins = Project("plugins", file(".")).settings(
     logLevel := Level.Warn,
-    resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
-    addSbtPlugin("play" % "sbt-plugin" % "2.2.1")
+    resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+    addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.2.1"),
     addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.2.1"),
     addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.2"),
     addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.2"))
